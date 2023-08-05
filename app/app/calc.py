2bfff5a3592420b53ebb3534072348622f3096f7
Melto007 <meltosm8@gmail.com>
2023-08-05 20:31:48 +0530

    update calc and tests files

--- app/app/calc.py
+++ app/app/calc.py
@@ -2,9 +2,7 @@
 Add two numbers
 """
 def add(x, y):
-    """ Add x and y and return result """
     return x + y
 
 def sub(x, y):
-    """ Sub x and x and return result """
     return x - y
--- app/app/tests.py
+++ app/app/tests.py
@@ -1,18 +1,14 @@
 """
 Unit Test
 """
-
 from django.test import SimpleTestCase
 from app import calc
 
 class CalcTest(SimpleTestCase):
-    """ Test the calc module """
     def test(self):
-        """ Test adding number together """
         res = calc.add(5, 5)
         self.assertEqual(res, 10)
 
     def test_sub(self):
-        """ Test sub number together """
         res = calc.sub(5, 5)
         self.assertEqual(res, 0)
