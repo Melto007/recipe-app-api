c3261640663955cfb7d00fc7695ebb40a5dbada5
Melto007 <meltosm8@gmail.com>
2023-08-27 13:25:02 +0530

    test normalized email and super user is created

--- app/core/management/commands/wait_for_db.py
+++ app/core/management/commands/wait_for_db.py
@@ -12,13 +12,13 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         """Entry Point for commands."""
-        self.stdout.write("waiting for database...")
+        self.stdout.write('Waiting for database....')
         db_up = False
         while db_up is False:
             try:
                 self.check(databases=['default'])
                 db_up = True
             except (psycopg2Error, OperationalError):
-                self.stdout.write("Database unavailable, waiting 1 second...")
+                self.stdout.write('DB is not connected')
                 time.sleep(1)
-        self.stdout.write(self.style.SUCCESS('Database Available'))
+        self.stdout.write(self.style.SUCCESS("DB is connected"))
--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -19,3 +19,30 @@ class ModelTests(TestCase):
 
         self.assertEqual(user.email, email)
         self.assertTrue(user.check_password(password))
+
+    def test_for_normalized_email(self):
+        """test for normalized email"""
+        sample_email = [
+            ['test1@EXAMPLE.com', 'test1@example.com'],
+            ['Test2@Example.com', 'Test2@example.com'],
+            ['TEST3@EXAMPLE.COM', 'TEST3@example.com'],
+            ['test4@example.COM', 'test4@example.com'],
+        ]
+
+        for email, expected in sample_email:
+            user = get_user_model().objects.create_user(email, 'sample123')
+            self.assertEqual(user.email, expected)
+
+    def test_new_user_without_email_error(self):
+        """test email raises error without email"""
+        with self.assertRaises(ValueError):
+            get_user_model().objects.create_user('', 'test123')
+
+    def test_create_superuser(self):
+        """test create superuser"""
+        user = get_user_model().objects.create_superuser(
+            'test@example.com',
+            'test123'
+        )
+        self.assertTrue(user.is_superuser)
+        self.assertTrue(user.is_staff)
