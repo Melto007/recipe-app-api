dbbd4039b2f24e8f69a5518efc80f1e7509b26fa
Melto007 <meltosm8@gmail.com>
2023-08-27 09:54:47 +0530

    changes in command test and wait for db

--- app/core/tests/test_commands.py
+++ app/core/tests/test_commands.py
@@ -1,13 +1,11 @@
 """
 Test custom management commands.
 """
+from django.test import SimpleTestCase
 from unittest.mock import patch
-
-from psycopg2 import OperationalError as psycopg2Error
-
 from django.core.management import call_command
 from django.db.utils import OperationalError
-from django.test import SimpleTestCase
+from psycopg2 import OperationalError as psycopg2Error
 
 
 @patch('core.management.commands.wait_for_db.Command.check')
@@ -17,13 +15,11 @@ class CommandTest(SimpleTestCase):
     def test_wait_for_db_ready(self, patched_check):
         """Test waiting for database is ready"""
         patched_check.return_value = True
-
         call_command('wait_for_db')
-
         patched_check.assert_called_once_with(databases=['default'])
 
     @patch('time.sleep')
-    def test_wait_for_db_delay(self, patched_sleep, patched_check):
+    def test_wait_for_db_error(self, patched_sleep, patched_check):
         """Test waiting for getting OperationalError."""
         patched_check.side_effect = [psycopg2Error] * 2 + \
             [OperationalError] * 3 + [True]
